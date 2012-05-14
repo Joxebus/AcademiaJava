ee673d806d1ee7af8e3aba1bc391940376a0fa1b
Omar Bautista <jbautista@sintelti.com.mx>
2012-05-14 13:54:59 -0500

    Control de flujos y lectura de archivos

---
+++ 6.- Control de Flujo/ControlDeFlujo.java
@@ -0,0 +1,24 @@
+enum Color { rojo, verde, azul }
+
+class ControlDeFlujo {
+
+	public static void main(String... args){
+		Color color = Color.verde;
+		switch(color){
+			case rojo:
+				System.out.println("Es rojo");
+			break;
+			case verde:
+				System.out.println("Es verde");
+			
+
+			case azul:
+				System.out.println("Es azul");
+			break;
+
+			default:
+				System.out.println("No se sabe el color...");
+			break;
+		}
+	}
+}
---
+++ 6.- Control de Flujo/LeerArchivo.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+class LeerArchivo {
+
+	public static void main(String... args){
+		try{
+			File archivo = new File("BreakContinue.java");
+			FileReader fr = new FileReader(archivo);
+			BufferedReader br = new BufferedReader(fr);
+			int noLinea = 0;
+			String linea;
+			while((linea = br.readLine()) != null){
+				if(linea.contains("continue")){
+					System.out.println(noLinea+": "+linea);
+					System.out.println("La linea "+noLinea+" tiene "+linea.split(" ").length+" palabras.");
+				}				
+				noLinea++;
+			}
+			fr.close();
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+		
+	}
+
+}
