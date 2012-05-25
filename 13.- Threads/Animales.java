5d9544fc4ebbd698ad72456a6978ce1faace76c7
Omar Bautista <jbautista@sintelti.com.mx>
2012-05-25 08:58:12 -0500

    Ejemplo sencillo de Threads

---
+++ 13.- Threads/Animales.java
@@ -0,0 +1,8 @@
+public interface Animales{
+
+	void comer();
+	void dormir();
+	void desplazarse();
+	void emitirSonido();
+
+}
---
+++ 13.- Threads/Perro.java
@@ -0,0 +1,40 @@
+public class Perro extends Terrestres implements Comparable, Runnable{
+	private int edad;
+	private String raza;
+
+	public void run(){
+		try{
+			Thread.sleep((long)(Math.random()*10000));
+			System.out.println("Me llamo "+this.getNombre());	
+			Thread.sleep((long)(Math.random()*5000));
+			System.out.println("Y tengo "+edad+" años");
+			System.out.println("Soy de tipo "+this.getClass().getName());	
+		}catch(InterruptedException ie){
+			System.out.println("Se detuvo la ejecución del Hilo");
+		}
+	}
+
+	public void setEdad(int edad){ this.edad = edad; }
+
+	public int getEdad(){ return edad; }
+
+	public String getRaza(){ return raza; }
+	public void setRaza(String raza){ this.raza = raza; }
+
+	public void desplazarse(){
+		System.out.println("Estoy comiendo y me llamo "+getNombre());
+	}
+
+	public void emitirSonido(){
+		System.out.println("Estoy durmiendo y me llamo "+getNombre());
+	}
+
+	public int compareTo(Object objeto){
+		if( objeto instanceof Perro ){
+			Perro perro = (Perro)objeto;
+			return this.raza.compareTo(perro.getRaza());
+		}else{
+			return -1;
+		}
+	}
+}
