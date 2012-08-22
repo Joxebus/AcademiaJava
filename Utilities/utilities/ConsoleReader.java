b2a8bed88895166ae8216a80bea63d4246eec087
Omar Bautista <jbautista@sintelti.com.mx>
2012-08-22 15:10:59 -0500

    Se agrega clase utilitaria para leer de consola

---
+++ Utilities/utilities/ConsoleReader.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class ConsoleReader {
+
+	private BufferedReader br;					
+
+	public ConsoleReader(){
+		br = new BufferedReader(new InputStreamReader(System.in));	
+	}
+
+	/**
+	* Este método regresa una cadena leida desde la consola, cada vez que es invocada
+	* si se genera una excepción regresa una cadena vacía.
+	*/
+	public String readLine(){		
+		try{
+			return br.readLine();		
+		}catch(IOException ioe){
+			System.out.println(ioe.getMessage());
+			return "";
+		}
+	}
+}
